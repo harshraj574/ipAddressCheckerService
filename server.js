9e73d91e93772282a1ac3bc7bee723ba544cd906
harshraj574 <harshraj574@gmail.com>
2024-11-20 23:58:01 +0530

    1st commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express  = require('express');
+const ip = require('ip');
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    const serverIp = ip.address();
+    return res.json({
+        ping: 'Pong',
+        message: serverIp
+    })
+
+
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on Port 3000");
+})
